4f8b830780781a4930f44eeaf240dc69925ee3ce
whitecream01 <whitecream01@gmail.com>
2016-04-02 19:31:48 +0200

    Version 1.1.1: Fixed: Beeg (thanks to youtube-dl) Fixed: PornAQ videos (support for Openload) Added: Option to use the thumbnails as poster/fanart

--- plugin.video.uwc/beeg.py
+++ plugin.video.uwc/beeg.py
@@ -23,6 +23,13 @@ import json
 
 import utils
 
+# from youtube-dl
+from compat import (
+    compat_chr,
+    compat_ord,
+    compat_urllib_parse_unquote,
+)
+
 dialog = utils.dialog
 
 # 80 BGMain
@@ -32,9 +39,9 @@ dialog = utils.dialog
 # 84 BGSearch
 
 def BGMain():
-    utils.addDir('[COLOR hotpink]Categories[/COLOR]','http://beeg.com/api/v5/index/main/0/pc',83,'','')
-    utils.addDir('[COLOR hotpink]Search[/COLOR]','http://beeg.com/api/v5/index/search/0/pc?query=',84,'','')
-    BGList('http://beeg.com/api/v5/index/main/0/pc')
+    utils.addDir('[COLOR hotpink]Categories[/COLOR]','http://api2.beeg.com/api/v6/1739/index/main/0/pc',83,'','')
+    utils.addDir('[COLOR hotpink]Search[/COLOR]','http://api2.beeg.com/api/v6/1739/index/main/0/pc?query=',84,'','')
+    BGList('http://api2.beeg.com/api/v6/1739/index/main/0/pc')
     xbmcplugin.endOfDirectory(utils.addon_handle)
 
 
@@ -44,11 +51,11 @@ def BGList(url):
 
     for videos in jsondata["videos"]:
         img = "http://img.beeg.com/236x177/" + videos["id"] +  ".jpg"
-        videopage = "http://beeg.com/api/v5/video/" + videos["id"]
+        videopage = "https://api.beeg.com/api/v6/1738/video/" + videos["id"]
         name = videos["title"].encode("utf8")
         utils.addDownLink(name, videopage, 82, img, '')
     try:
-        page=re.compile('http://beeg.com/api/v5/index/[^/]+/([0-9]+)/pc', re.DOTALL | re.IGNORECASE).findall(url)[0]
+        page=re.compile('http://api2.beeg.com/api/v6/1739/index/[^/]+/([0-9]+)/pc', re.DOTALL | re.IGNORECASE).findall(url)[0]
         page = int(page)
         npage = page + 1
         if jsondata["pages"] > page:
@@ -57,30 +64,35 @@ def BGList(url):
     except: pass
     xbmcplugin.endOfDirectory(utils.addon_handle)
 
+# from youtube-dl   
+def split(o, e):
+    def cut(s, x):
+        n.append(s[:x])
+        return s[x:]
+    n = []
+    r = len(o) % e
+    if r > 0:
+        o = cut(o, r)
+    while len(o) > e:
+        o = cut(o, e)
+    n.append(o)
+    return n
 
 def decrypt_key(key):
-    a = '5ShMcIQlssOd7zChAIOlmeTZDaUxULbJRnywYaiB'
-    e = urllib2.unquote(key).decode('utf-8')
-    t = len(a)
-    o = ''
-    for n in range(len(e)):
-        r = ord(e[n])
-        i = n % t
-        s = ord(a[i]) % 21
-        o += chr(r - s)
-    ofinal = ''
-    while len(o) > 3:
-        oPart = o[-3:]
-        o = o[:-3]
-        ofinal = ofinal+oPart
-    ofinal = ofinal+o
-    return ofinal
-
+    # Reverse engineered from http://static.beeg.com/cpl/1738.js
+    a = 'GUuyodcfS8FW8gQp4OKLMsZBcX0T7B'
+    e = compat_urllib_parse_unquote(key)
+    o = ''.join([
+        compat_chr(compat_ord(e[n]) - compat_ord(a[n % len(a)]) % 21)
+        for n in range(len(e))])
+    return ''.join(split(o, 3)[::-1])   
+##
 
 def BGPlayvid(url, name, download=None):
-    videopage = utils.getHtml(url,'')
+    videopage = utils.getHtml(url,'http://beeg.com')
     videopage = json.loads(videopage)
     
+   
     if not videopage["240p"] == None:
         url = videopage["240p"].encode("utf8")
     if not videopage["480p"] == None:
@@ -88,7 +100,8 @@ def BGPlayvid(url, name, download=None):
     if not videopage["720p"] == None:
         url = videopage["720p"].encode("utf8")
 
-    url = url.replace("{DATA_MARKERS}","data=pc.DE")
+
+    url = url.replace("{DATA_MARKERS}","data=pc_XX")
     if not url.startswith("http:"): url = "https:" + url
     
     key = re.compile("/key=(.*?)%2Cend", re.DOTALL | re.IGNORECASE).findall(url)[0]
@@ -118,7 +131,7 @@ def BGCat(url):
     catjson = json.loads(caturl)
     
     for tag in catjson["tags"]["popular"]:
-        videolist = "http://beeg.com/api/v5/index/tag/0/mobile?tag=" + tag.encode("utf8")
+        videolist = "http://api2.beeg.com/api/v6/1739/index/tag/0/mobile?tag=" + tag.encode("utf8")
         name = tag.encode("utf8")
         name = name[:1].upper() + name[1:]
         utils.addDir(name, videolist, 81, '')
