5702996553531eb13e7e027e7211cd5ae832540e
WhitecreamDev <35774656+WhitecreamDev@users.noreply.github.com>
2018-02-24 20:17:52 +0000

    Update default.py

--- plugin.video.uwc/default.py
+++ plugin.video.uwc/default.py
@@ -135,6 +135,7 @@ def INDEXW():
 @utils.url_dispatcher.register('3')
 def INDEXH():
     utils.addDir('[COLOR hotpink]Hentaihaven[/COLOR]','http://hentaihaven.org/?sort=date',460,os.path.join(imgDir, 'hh.png'),'')
+    utils.addDir('[COLOR hotpink]Animeid Hentai[/COLOR]','https://animeidhentai.com/hentai',660,os.path.join(imgDir, 'ah.png'),'')
     xbmcplugin.endOfDirectory(utils.addon_handle, cacheToDisc=False)    
 
 @utils.url_dispatcher.register('5', ['page'])
